2081392bed8972cd65618e56e9e761a76acff449
Lucky-Asher-Mafuta <120241956+Lucky-Asher-Mafuta@users.noreply.github.com>
2022-12-12 22:08:58 +0530

    Add files via upload

---
+++ currency_converter.py
@@ -0,0 +1,7 @@
+print("hello")
+sc=str(input("Enter the source currency:"))
+cr=float(input("Enter conversion rate:"))
+tc=str(input("Enter target currency"))
+amnt=float(input())
+if sc=="" or cr=="" or tc=="" or amnt=="":
+    print("please enter a value")
